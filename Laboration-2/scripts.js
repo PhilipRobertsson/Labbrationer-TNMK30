6fa544b1f1cae977c7929018bf4f5418818da445
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2021-11-05 11:57:31 +0100

    added timer js

---
+++ Laboration-2/scripts.js
@@ -0,0 +1,28 @@
+var timerON = null;
+var uppdate = null;
+//This part of the code initializes everything needed for the timer
+function liveClock(){
+	var d = new Date();
+	var t = d.toLocaleTimeString();
+	document.getElementById("liveClock").innerHTML = t;
+}
+//This part of the code uppdates the live clock
+function stopLiveCock() {
+    if(timerOn){
+        clearInterval(uppdate);
+        timerOn = false;
+    }
+}
+//This part stops the live clock if the clock is active
+function startLiveCock() {
+if(!timerOn){
+    uppdate = setInterval(liveClock, 1000);
+    timerOn = true;
+    }
+}
+
+
+function homeLoad(){
+    timerOn = true;
+	uppdate = setInterval(liveClock, 1000);
+}
