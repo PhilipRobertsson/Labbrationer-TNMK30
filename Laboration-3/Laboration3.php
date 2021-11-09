fc336808d7d18773c23662376e83265d05397aeb
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2021-11-09 16:58:45 +0100

    php prep

---
+++ Laboration-3/Laboration3.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<head>
+    <meta name='Author' content='Max Wiklundh & Philip Robertsson'>
+    <meta name='Description' content='Laboration 1'>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laboration 2</title>
+    <link rel="stylesheet" media="screen" href="styles.css" type="text/css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+
+    <script src="scripts.js"></script>
+</head>
+
+<body>
+    <?php
+    echo "My first PHP script!";
+    ?>
+
+</body>
