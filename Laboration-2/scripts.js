77a2621afc874225fb9dd4e8e8999bbabf445181
PhilipRobertsson <phiro138@student.liu.se>
2021-11-09 14:03:21 +0100

    test

--- Laboration-2/scripts.js
+++ Laboration-2/scripts.js
@@ -84,10 +84,10 @@ function changeP(){
        list[i].style.fontFamily = "'Press Start 2P', cursive";
        list[i].style.fontSize = "larger";
    }
+   var bokInfo = document.getElementById("bokInfo");
+   for (var i = 0; i < bokInfo.length; i++)
+   document.getElementById("bokInfo").children.style.fontFamily = "'Press Start 2P', cursive";
    
-   
-    //document.getElementsByTagName("p").forEach(element => {
-       // element.style.fontFamily = "'Press Start 2P', cursive"});
 }
 
 function loadStyle(){
