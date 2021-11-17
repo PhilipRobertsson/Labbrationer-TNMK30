22ffee5c17e24fd75d128366b4130f2ab34cf941
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2021-11-17 13:17:53 +0100

    basic framework

--- laboration5/lego.php
+++ laboration5/lego.php
@@ -16,5 +16,31 @@
         if(!$connection){
             die('MySQL connection error');
         }
+
+        $sql_query = "SELECT * FROM phiro138 ORDER BY entry_date DESC LIMIT 20";
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+        }
+
+        $result = mysqli_query($connection, $sql_query);
+
+        while ($row = mysqli_fetch_array($result)){
+
+
+
+            /*
+            $heading = $row['entry_heading'];
+            print("<h3>$heading</h3>");
+            $author =$row['entry_author'];
+            $date = $row['entry_date'];
+            print("<p>$author</p>");
+            print("<small>$date</small>");
+            $text = $row['entry_text'];
+            print("<p>$text</p>");
+            print("<hr/>");
+            */
+        }// end while
+        mysqli_close($connection);
     ?>
 </body>
