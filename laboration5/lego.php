7c7b1a05624770c79790ec0bf00483f4efc8066c
PhilipRobertsson <phiro138@student.liu.se>
2021-11-17 13:09:37 +0100

    lol

---
+++ laboration5/lego.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<head>
+    <meta name='Author' content='Max Wiklundh & Philip Robertsson'>
+    <meta name='Description' content='Laboration 5'>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laboration 3</title>
+    <link rel="stylesheet" media="screen" href="styles.css" type="text/css">
+
+    <script src="scripts.js"></script>
+</head>
+<head>
+    
+</head>
